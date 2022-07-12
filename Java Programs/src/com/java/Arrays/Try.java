74e8915306b9f8375471a1cea9eba9d857a94e53
Dhiraj-Pote <dhirajpote29@gmail.com>
2022-07-12 21:33:55 +0530

    initial commit

--- Java Programs/src/com/java/Arrays/Try.java
+++ Java Programs/src/com/java/Arrays/Try.java
@@ -1,21 +1,21 @@
 package com.java.Arrays;
 
-import java.util.Arrays;
-
 public class Try {
     public static void main(String[] args) {
-        int[] nums = { 2,5,5,11 };
-        int target = 10;
-        System.out.println(Arrays.toString(twoSum(nums,target)));
-    }
-    static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if( i!=j && nums[i] + nums[j] == target)
-                    return new int[] {i,j};
-            }
-        }
-        return new int[] {-1,-1};
+        System.out.println(mySqrt(69));
     }
 
+    static int mySqrt(int x) {
+        long s = 0, e = x, ans =0;
+        while(s<=e){
+            long m = s + (e-s)/2 ;
+            if(m*m<=x){
+                ans=m;
+                s = m+1;
+            }
+            else
+                e = m-1;
+        }
+        return (int)ans;
+    }
 }
