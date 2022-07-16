e2f9775f1014307320951e849595564ab319d9ff
Dhiraj-Pote <dhirajpote29@gmail.com>
2022-07-16 22:32:47 +0530

    initial commit

---
+++ Java Programs/src/com/java/Basics/FiboFormula.java
@@ -0,0 +1,21 @@
+package com.java.Basics;
+
+// 1/sqrt(5){ ((1 + sqrt(5))/2)^n + ((1 - sqrt(5))/2)^n } -> n th fibo num.
+
+public class FiboFormula {
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            System.out.println(fibo(i));
+        }
+    }
+
+    static int fibo(int n) {
+        double sq = Math.sqrt(5);
+        if(n<2) return n;
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+
+        return (int) ((( Math.pow(((1 + sq)/2), n) ) - ( Math.pow(((1 - sq)/2), n) ) )/sq);
+    }
+
+}
--- Java Programs/src/com/java/Recursion/fibonacci.java
+++ Java Programs/src/com/java/Recursion/fibonacci.java
@@ -2,9 +2,9 @@ package com.java.Recursion;
 
 public class fibonacci {
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(fibo(n));  // num:  0 1 1 2 3 5  8  13  21 34 55
-                                      // ind:  0 1 2 3 4 5  6  7   8  9  10
+        int n = 45;
+        System.out.println(fibo(n));  // num:  0 1 1 2 3 5  8  13  21 34 55 89
+                                      // ind:  0 1 2 3 4 5  6  7   8  9  10 11
     }
 
     static int fibo(int n) {
