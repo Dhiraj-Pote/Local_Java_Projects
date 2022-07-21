49dd25b6ac8b39159849d87357ac1d0d7148d013
Dhiraj-Pote <dhirajpote29@gmail.com>
2022-07-21 12:40:52 +0530

    initial commit

--- Java Programs/src/com/java/Basics/CountPrimes.java
+++ Java Programs/src/com/java/Basics/CountPrimes.java
@@ -1,4 +1,29 @@
 package com.java.Basics;
 
+//https://leetcode.com/problems/count-primes/
+// Given an integer n, return the number of prime numbers that are strictly less than n.
 public class CountPrimes {
+    public static void main(String[] args) {
+        int n = 30;
+        boolean[] prime = new boolean[n + 1];
+        System.out.println(isPrime(n, prime));
+    }
+
+    static int isPrime(int n, boolean[] prime) { // Sieve of Eratosthenes.
+        int count = 0;
+
+        if (n == 0 || n == 1) return 0;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (!prime[i]) {
+                for (int j = 2 * i; j < n; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (!prime[i]) count++;
+        }
+        return count;
+    }
 }
--- Java Programs/src/com/java/BitWise/noOf_1.java
+++ Java Programs/src/com/java/BitWise/noOf_1.java
@@ -2,13 +2,13 @@ package com.java.BitWise;
 
 public class noOf_1 {
     public static void main(String[] args) {
-        System.out.println(hammingWeight(9));
+        System.out.println(hammingWeight(11)); // 1011
     }
     static int hammingWeight(int n) {
         int count = 0;
         while(n!=0){
-            count+=(n & 1);
-            n = n>>1;
+            if((n & 1) ==1) count++;  // +1 every time, when lsb is one
+            n = n>>1;  // right shift
         }
         return count;
     }
